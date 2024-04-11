b3eecca5a64b097ec71d3a19b09e89eacf2a0890
георгий савинцев <igsgo4645@gmail.com>
2024-04-11 22:49:46 +0400

    add func capital_letter

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def large_letters(word):
+    """Функция возвращает слово из заглавных букв"""
     return word.upper()
+
+
+def capital_letter(word):
+    """Функция возвращает слово с первой заглавной буквой"""
+    word = ' '.join(word.capitalize() for word in word.split())
+    return word
